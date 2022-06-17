35aa32287d7f18edbc5e0cb48e0e50e93f612fc1
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-06-17 14:39:27 +0900

    Initialize.

---
+++ Lasagna.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let expectedMinutesInOven = 40
+
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    expectedMinutesInOven - elapsedMinutes
+}
+
+func preparationTimeInMinutes(layers: Int) -> Int {
+    layers * 2
+}
+
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    preparationTimeInMinutes(layers: layers) + elapsedMinutes
+}
